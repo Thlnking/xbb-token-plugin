0c5bd329c6cad9de75bf624f37f0b86fccec9e44
周鹏武 <pengwu.zhou@xbongbong.com>
2023-12-22 23:36:47 +0800

    feat: add ace editor

--- src/components/TokenInput/index.jsx
+++ src/components/TokenInput/index.jsx
@@ -4,11 +4,25 @@ import { useState } from "react";
 import { Card, CardHeader, CardBody, CardFooter, Avatar, Button, ButtonGroup } from "@nextui-org/react";
 import { stylex } from "@stylexjs/stylex";
 import { cardStyles } from "./style";
+import AceEditor from "react-ace";
+import ace from "ace-builds";
+import workerJavascriptUrl from "ace-builds/src-noconflict/worker-javascript?url";
+
+import "ace-builds/src-noconflict/mode-json";
+import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/ext-language_tools";
+import { useEffect } from "react";
+ace.config.set('basePath', '/node_modules/ace-builds/src-min-noconflict');
+ace.config.setModuleUrl('ace/mode/javascript_worker', workerJavascriptUrl);
 
 
 const TokenInput = () => {
 
     const [isFollowed, setIsFollowed] = useState(false);
+
+    useEffect(() => {
+        console.log('[ ace ] >', ace)
+    }, [])
 
     return (
         <>
@@ -18,7 +32,7 @@ const TokenInput = () => {
                 ...stylex.props(cardStyles.card)
                 }
             >
-                <CardHeader className="justify-between">
+                {/* <CardHeader className="justify-between">
                     <div className="flex gap-5">
                         <div className="flex flex-col gap-1 items-start justify-center">
                             <h4 className="text-small font-semibold leading-none text-default-600">Zoey Lang</h4>
@@ -47,8 +61,27 @@ const TokenInput = () => {
                             保存
                         </Button>
                     </ButtonGroup>
-                </CardHeader>
-                <CardBody className="px-4 py-0 h-36 text-small text-default-400">
+                </CardHeader> */}
+                <CardBody className="px-4 py-0 h-56 text-small text-default-400">
+                    <AceEditor
+                        value="{}"
+                        mode="json"
+                        theme="tomorrow"
+                        onChange={(value) => {
+                            console.log('[ value ] >', value)
+                        }}
+                        name="token-json-edit"
+                        editorProps={{ $blockScrolling: true }}
+                        fontSize={14}
+                        showPrintMargin={true}
+                        showGutter={true}
+                        highlightActiveLine={true}
+                        setOptions={{
+                            enableBasicAutocompletion: true,
+                            enableSnippets: true,
+                            enableLiveAutocompletion: true
+                        }}
+                    />
                 </CardBody>
             </Card>
         </>
