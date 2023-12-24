c7a55bd90a346f08a86800e478d5eaae005acb6a
周鹏武 <pengwu.zhou@xbongbong.com>
2023-12-24 23:05:45 +0800

    feat: ace editor

--- src/components/TokenInput/index.jsx
+++ src/components/TokenInput/index.jsx
@@ -1,7 +1,7 @@
 
 import { useState } from "react";
 
-import { Card, CardHeader, CardBody, CardFooter, Avatar, Button, ButtonGroup } from "@nextui-org/react";
+import { Card, CardBody, Button, ButtonGroup } from "@nextui-org/react";
 import { stylex } from "@stylexjs/stylex";
 import { cardStyles } from "./style";
 import AceEditor from "react-ace";
@@ -12,14 +12,26 @@ import "ace-builds/src-noconflict/mode-json";
 import "ace-builds/src-noconflict/theme-github";
 import "ace-builds/src-noconflict/ext-language_tools";
 import { useEffect } from "react";
+import { useRef } from "react";
 ace.config.set('basePath', '/node_modules/ace-builds/src-min-noconflict');
 ace.config.setModuleUrl('ace/mode/javascript_worker', workerJavascriptUrl);
 
 
+
+
 const TokenInput = () => {
 
-
+    const isJson = (str) => {
+        try {
+            JSON.parse(str);
+        } catch (e) {
+            return false;
+        }
+        return true;
+    }
     const [validate, setValidate] = useState(true)
+    const [inputValue, setInputValue] = useState('{}')
+    const editor = useRef(null)
 
     useEffect(() => {
         console.log('[ ace ] >', ace)
@@ -33,36 +45,6 @@ const TokenInput = () => {
                 ...stylex.props(cardStyles.card)
                 }
             >
-                {/* <CardHeader className="justify-between">
-                    <div className="flex gap-5">
-                        <div className="flex flex-col gap-1 items-start justify-center">
-                            <h4 className="text-small font-semibold leading-none text-default-600">Zoey Lang</h4>
-                            <h5 className="text-small tracking-tight text-default-400">@zoeylang</h5>
-                        </div>
-                    </div>
-                    <ButtonGroup>
-                        <Button
-                            onPress={() => setIsFollowed(!isFollowed)}
-                            className="text-tiny text-white bg-black/10"
-                            variant="flat"
-                            color="default"
-                            radius="lg"
-                            size="sm"
-                        >
-                            复制
-                        </Button>
-                        <Button
-                            onPress={() => setIsFollowed(!isFollowed)}
-                            className="text-tiny text-white bg-black/10"
-                            variant="flat"
-                            color="default"
-                            radius="lg"
-                            size="sm"
-                        >
-                            保存
-                        </Button>
-                    </ButtonGroup>
-                </CardHeader> */}
                 <CardBody className="px-0 py-0 h-60 text-small text-default-400">
                     <div>
                         <ButtonGroup className="w-full ">
@@ -87,18 +69,22 @@ const TokenInput = () => {
                         </ButtonGroup>
                     </div>
                     <AceEditor
+                        ref={editor}
+                        value={inputValue}
                         width="100%"
                         height="100%"
                         mode="json"
                         theme="tomorrow"
-                        onChange={(value, event) => {
-                            console.log('[ value ] >', value, event)
-                        }}
                         onValidate={(annotations) => {
                             console.log('[ annotations ] >', annotations)
                             setValidate(annotations.length === 0)
                         }}
-
+                        onPaste={(value) => {
+                            if (isJson(value)) {
+                                setInputValue(JSON.stringify(JSON.parse(value), null, 4))
+                            }
+                        }}
+                        enableBasicAutocompletion
                         name="token-json-edit"
                         editorProps={{ $blockScrolling: true }}
                         fontSize={14}
